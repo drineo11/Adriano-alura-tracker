d6d4f7fffe26debd1d0f9fb77c54b3de9bdf1750
Adriano Castro <drineo@hotmail.com>
2022-09-07 18:05:56 -0300

    Alura_Tracker_v.3

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,68 +1,43 @@
-import IProjeto from "@/interfaces/IProjeto";
 import { createStore, Store, useStore as vuexUseStore } from "vuex";
 import { InjectionKey } from 'vue'
-import { ADICIONA_PROJETO, ALTERA_PROJETO, EXCLUIR_PROJETO, NOTIFICAR } from "./tipo-mutacoes";
+import { NOTIFICAR } from "./tipo-mutacoes";
 import { INotificacao } from "@/interfaces/INotificacao";
 
-interface Estado {
-    projetos: IProjeto[],
-    notificacoes: INotificacao[]
+import { EstadoProjeto, projeto } from "./modulos/projeto";
+import { EstadoTarefa, tarefa } from "./modulos/tarefas";
+
+export interface Estado {
+    notificacoes: INotificacao[],
+    projeto: EstadoProjeto,
+    tarefa: EstadoTarefa
 }
 
 export const key: InjectionKey<Store<Estado>> = Symbol()
 
 export const store = createStore<Estado>({
     state: {
-        projetos: [],
-        notificacoes: []
-        // {
-        //     id: 1,
-        //     texto: 'Uma notificação de sucesso',
-        //     titulo: 'Sucesso',
-        //     tipo: TipoNotificacao.SUCESSO
-
-        // },
-        // {
-        //     id: 1,
-        //     texto: 'Uma notificação de atencao',
-        //     titulo: 'Atenção',
-        //     tipo: TipoNotificacao.ATENCAO
-
-        // },
-        // {
-        //     id: 1,
-        //     texto: 'Uma notificação de falha',
-        //     titulo: 'Falha',
-        //     tipo: TipoNotificacao.FALHA
-
-        // },
-
-
+        notificacoes: [],
+        projeto: {
+            projetos: []
+        },
+        tarefa: {
+            tarefas: []
+        }
     },
     mutations: {
-        [ADICIONA_PROJETO](state, nomeDoProjeto: string) {
-            const projeto = {
-                id: new Date().toISOString(),
-                nome: nomeDoProjeto
-            } as IProjeto
-            state.projetos.push(projeto)
-        },
-        [ALTERA_PROJETO](state, projeto: IProjeto) {
-            const index = state.projetos.findIndex(proj => proj.id == projeto.id)
-            state.projetos[index] = projeto
-        },
-        [EXCLUIR_PROJETO](state, id: string) {
-            state.projetos = state.projetos.filter(proj => proj.id != id)
-        },
-        [NOTIFICAR](state, novaNotificacao: INotificacao) {
+        [NOTIFICAR] (state, novaNotificacao: INotificacao) {
+
             novaNotificacao.id = new Date().getTime()
             state.notificacoes.push(novaNotificacao)
 
             setTimeout(() => {
                 state.notificacoes = state.notificacoes.filter(notificacao => notificacao.id != novaNotificacao.id)
-            }, 2500)
-
+            }, 3000)
         }
+    },
+    modules: {
+        projeto,
+        tarefa
     }
 })
 
